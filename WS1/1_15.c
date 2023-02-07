df4b67035477a1f575f2f14ba57c08f12e3a3c86
TanishaD111 <tanishadamle111@gmail.com>
2023-02-07 01:24:35 +0000

    First commit

---
+++ WS1/1_15.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+
+   /* Type your code here. */
+   printf("Hello World!");
+   printf("\n");
+   
+   return 0;
+}
---
+++ WS1/1_16.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+   /* Type your code here. */
+   printf("  NO PARKING");
+   printf("\n");
+   printf("2:00 - 6:00 a.m.");
+   printf("\n");
+   
+   return 0;
+}
